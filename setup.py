933f4c68738199e59f54e89260ee03bb44b134f4
George Hickman <george@ghickman.co.uk>
2012-02-06 11:28:01 +0000

    Use single quotes

--- setup.py
+++ setup.py
@@ -1,13 +1,16 @@
 from setuptools import setup, find_packages
 
 from countries import get_version
+
+
 setup(
-    name = "incuna-countries",
+    name = 'incuna-countries',
     packages = find_packages(),
     include_package_data=True,
     version = get_version(),
-    description = "Countries model and fixtures.",
-    author = "Incuna Ltd",
-    author_email = "admin@incuna.com",
-    url = "http://incuna.com/",
+    description = 'Countries model and fixtures.',
+    author = 'Incuna Ltd',
+    author_email = 'admin@incuna.com',
+    url = 'http://incuna.com/',
 )
+
