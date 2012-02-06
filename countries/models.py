214502c99b2b7f610fea30f4d8eb0ed7e3e41a1b
George Hickman <george@ghickman.co.uk>
2012-02-06 11:28:21 +0000

    Order file correctly

--- countries/models.py
+++ countries/models.py
@@ -1,10 +1,14 @@
 from django.db import models
 
+
 class Country(models.Model):
     name = models.CharField(max_length=200)
     code = models.CharField(max_length=2, unique=True)
+
+    class Meta:
+        ordering = ('name',)
+        verbose_name_plural = 'countries'
+
     def __unicode__(self):
         return self.name
-    class Meta:
-        ordering = ['name']
-        verbose_name_plural = "countries"
+
