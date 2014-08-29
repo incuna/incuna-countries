3ba7a6a394cc9d2a5620a0e0ab0dd391604f95b8
Adam Thomas <sortoflikechess@gmail.com>
2014-08-29 16:18:02 +0100

    Use __str__ and a decorator instead of __unicode__.

--- countries/models.py
+++ countries/models.py
@@ -1,6 +1,10 @@
+from __future__ import unicode_literals
+
 from django.db import models
+from django.utils.encoding import python_2_unicode_compatible
 
 
+@python_2_unicode_compatible
 class Country(models.Model):
     name = models.CharField(max_length=200)
     code = models.CharField(max_length=2, unique=True)
@@ -9,6 +13,5 @@ class Country(models.Model):
         ordering = ('name',)
         verbose_name_plural = 'countries'
 
-    def __unicode__(self):
+    def __str__(self):
         return self.name
-
